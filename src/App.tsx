83432bb7f8f09e609f7ce861ffaaa196d3176b26
Bruno <brunonoriller@gmail.com>
2024-09-01 13:37:40 -0300

    adding title

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { Board } from './components/board';
 
 export default function App() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-gray-900 p-4">
-      <div className="rainbow-shadow relative z-0 aspect-square max-h-[90vmin] w-full max-w-[90vmin]">
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-900 p-4">
+      <h1 className="mb-8 text-3xl font-bold text-white">
+        Chunk-Busters: Don't cross the streams!
+      </h1>
+      <div className="rainbow-shadow relative z-0 aspect-square h-full max-h-[90vmin] max-w-[90vmin]">
         <Board />
       </div>
     </div>
