328952b980f839788122967239c46ff91ffc1531
Bruno <brunonoriller@gmail.com>
2024-09-15 12:06:41 -0300

    improve hook

--- src/components/board/useBoards.tsx
+++ src/components/board/useBoards.tsx
@@ -1,31 +1,52 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+
+type Indexes = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
 const random = () => Math.random() > 0.5;
 
-function makeLights<T = boolean>(
-  // @ts-expect-error
-  fn: () => T = random,
-) {
+function makeLights<T = boolean>(fn?: () => T): Record<Indexes, T>;
+function makeLights(fn = random) {
   return Object.fromEntries(
-    Array.from({ length: 9 }).map((_, i) => [i, fn()]),
-  ) as Record<number, T>;
+    Array.from({ length: 9 }).map((_, i) => [i as Indexes, fn()]),
+  );
 }
 
-const randomIndex = () => Math.floor(Math.random() * 9);
+const randomIndex = () => Math.floor(Math.random() * 9) as Indexes;
 
 export function useBoards() {
-  const [lights, setLights] = useState(makeLights(makeLights));
+  const { lights, toggle } = useLights(makeLights(makeLights));
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setLights((lights) => {
-        lights[randomIndex()][randomIndex()] = random();
-        return { ...lights };
-      });
+      toggle(randomIndex(), randomIndex(), random());
     }, 100);
 
     return () => clearInterval(interval);
   }, []);
 
   return lights;
+}
+
+function useLights(init: Record<Indexes, Record<Indexes, boolean>>) {
+  const [lights, setLights] = useState(init);
+
+  const toggle = useCallback((board: Indexes, light: Indexes, on?: boolean) => {
+    if (
+      board === undefined ||
+      light === undefined ||
+      board < 0 ||
+      board > 8 ||
+      light < 0 ||
+      light > 8
+    ) {
+      return;
+    }
+
+    return setLights((lights) => {
+      lights[board][light] = on ?? !lights[board][light];
+      return { ...lights };
+    });
+  }, []);
+
+  return { lights, toggle };
 }
