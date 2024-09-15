7a1f80334f7afc4baea0946714b6d048b1d8c55d
Bruno <brunonoriller@gmail.com>
2024-09-15 12:41:32 -0300

    improving navigation and buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
 import { Board } from './components/board';
 import { FancyTitle } from './components/fancyTitle';
-import { Button } from './components/ui/button';
 import { ScrollArea } from './components/ui/scroll-area';
 import { useNav } from './components/useNav';
 
 export default function BoardDemo() {
-  const { navItems, navContent, activeNav, changeActiveNav } = useNav();
+  const { navContent, NavButtons } = useNav();
 
   return (
-    <div className="block h-full min-h-screen bg-gray-900 text-white 2xl:flex 2xl:h-screen">
+    <div className="block h-full min-h-screen bg-primary text-primary-foreground 2xl:flex 2xl:h-screen">
       <div className="w-full">
         <div className="h-full overflow-auto pr-6 max-2xl:pt-16 lg:overflow-hidden">
-          <div className="left-0 top-0 z-10 w-full bg-gray-900 pl-4 pt-4 max-2xl:fixed">
+          <div className="left-0 top-0 z-10 w-full bg-primary pl-4 pt-4 max-2xl:fixed">
             <FancyTitle />
           </div>
           <ScrollArea className="h-full p-6 pt-10">
             <nav className="mb-4 flex flex-wrap gap-2">
-              {navItems.map((item) => (
-                <Button
-                  key={item.id}
-                  onClick={() => changeActiveNav(item.id)}
-                  variant={activeNav === item.id ? 'default' : 'outline'}
-                  className="mb-2"
-                >
-                  {item.title}
-                </Button>
-              ))}
+              <NavButtons />
             </nav>
-            <div className="mt-4 text-white">{navContent}</div>
+            <div className="mt-4 text-primary-foreground">{navContent}</div>
           </ScrollArea>
         </div>
       </div>
--- src/components/useNav.tsx
+++ src/components/useNav.tsx
@@ -1,15 +1,54 @@
 import { useState, useCallback } from 'react';
+import { Button } from './ui/button';
+
+function useSearchParams(spName: string) {
+  const sp = new URLSearchParams(window.location.search);
+  const get = () => sp.get(spName);
+
+  const set = useCallback(
+    (value: string | null) => {
+      if (value === null) {
+        sp.delete(spName);
+      } else {
+        sp.set(spName, value);
+      }
+
+      window.history.pushState(
+        {},
+        '',
+        `${window.location.pathname}?${sp.toString()}`,
+      );
+    },
+    [spName],
+  );
+
+  return [get, set] as const;
+}
+
+function useSearchParamsState(
+  spName: string,
+  defaultValue: string | null = null,
+) {
+  const [get, set] = useSearchParams(spName);
+  const [state, setState] = useState(get() || defaultValue);
+  const update = useCallback((value: string | null) => {
+    setState(value);
+    set(String(value));
+  }, []);
+
+  return [state, update] as const;
+}
 
 export function useNav() {
-  const [activeNav, setActiveNav] = useState('nav1');
+  const [activeNav, setActiveNav] = useSearchParamsState('nav', '1');
 
   const navItems = [
-    { id: 'nav1', title: 'Navigation 1', content: 'Content for Navigation 1' },
-    { id: 'nav2', title: 'Navigation 2', content: 'Content for Navigation 2' },
-    { id: 'nav3', title: 'Navigation 3', content: 'Content for Navigation 3' },
+    { id: 1, title: 'Navigation 1', content: 'Content for Navigation 1' },
+    { id: 2, title: 'Navigation 2', content: 'Content for Navigation 2' },
+    { id: 3, title: 'Navigation 3', content: 'Content for Navigation 3' },
     // Add more navigation items to demonstrate scrolling
     ...Array.from({ length: 10 }, (_, i) => ({
-      id: `nav${i + 4}`,
+      id: i + 4,
       title: `Navigation ${i + 4}`,
       content: `Content for Navigation ${i + 4}`,
     })),
@@ -47,11 +86,24 @@ export function useNav() {
     explicabo ullam dolor voluptate?
   `;
 
-  const changeActiveNav = useCallback((id: string) => {
+  const changeActiveNav = useCallback((id: number) => {
     if (navItems.find((item) => item.id === id)) {
-      setActiveNav(id);
+      setActiveNav(String(id));
     }
   }, []);
 
-  return { activeNav, changeActiveNav, navContent, navItems };
+  const NavButtons = useCallback(() => {
+    return navItems.map((item) => (
+      <Button
+        key={item.id}
+        onClick={() => changeActiveNav(item.id)}
+        variant={Number(activeNav) === item.id ? 'secondary' : 'ghost'}
+        className="mb-2"
+      >
+        {item.title}
+      </Button>
+    ));
+  }, [activeNav]);
+
+  return { navContent, NavButtons };
 }
